0abd434a8016fe3e00b58dff8acc85388a592cb6
CathGirL1 <catherinesonderegger446@gmail.com>
2025-06-12 00:48:11 -0300

    Guardando repositorio actualizado con restsharp y los modelos clima y cotizacion de asp.net

---
+++ obligatorioProg3/Models/Cotizacion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RestSharp;
+
+namespace obligatorioProg3.Models
+{
+    public class Cotizacion
+    {
+        public int id { get; set; }
+        public DateTime fecha { get; set; }
+        public string tipoMoneda { get; set; }
+        public float valor { get; set; }
+    }
+}
