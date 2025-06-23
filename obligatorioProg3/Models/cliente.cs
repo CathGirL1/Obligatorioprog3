3f05f9f7cf6ffeec764da00da9bda8e31e830177
Elias <fernandezvillaelias41@gmail.com>
2025-06-22 23:39:41 -0300

    pequeños cambios finales del registro para que funcione bien, todavía no estan hechas las validaciones

--- obligatorioProg3/Models/cliente.cs
+++ obligatorioProg3/Models/cliente.cs
@@ -26,7 +26,5 @@ namespace obligatorioProg3.Models
         public virtual usuario usuario { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<comentario> comentario { get; set; }
-
-        public virtual datosCliente_dependenciaCedula datosCliente_dependenciaCedula { get; set; }
     }
 }
