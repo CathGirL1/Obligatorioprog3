b84c282c630e66a291adbacc0145cccdf487f95e
Elias <fernandezvillaelias41@gmail.com>
2025-07-12 22:14:03 -0300

    ahora en la pagina de inicio se puede ver el programa actual y se guarda en la base de datos el clima actual de la api de clima

---
+++ obligatorioProg3/ViewModels/InicioViewModel.cs
@@ -0,0 +1,28 @@
+﻿using obligatorioProg3.CustomValidations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using obligatorioProg3.Models;
+
+namespace obligatorioProg3.ViewModels
+{
+    public class InicioViewModel
+    {
+        //noticias
+        public List<noticia> noticias { get; set; }
+
+
+        //Datos de programa
+        public int idPrograma { get; set; }
+        public string nombrePrograma { get; set; }
+        public string descripcionPrograma { get; set; }
+        public string imagenPrograma { get; set; }
+
+
+        //Datos de horarios de programas
+        public string dia { get; set; }
+        public System.TimeSpan horaInicio { get; set; }
+        public System.TimeSpan horaFinal { get; set; }
+    }
+}
